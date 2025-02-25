8c5b9d2e0ebbfd6db8661bb6d08a6fef19a6b9ae
smoyer-sl <smoyer@splinterlands.com>
2025-02-25 18:39:12 -0500

    fix test structure gen

--- scripts/create-structural-dump.sh
+++ scripts/create-structural-dump.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+
+# our version of pg_dump doesnt support excluding extensions,
+# so we have to exclude the extensions manually
+# just remove any lines that start with CREATE EXTENSION
+# from the dump file
 pg_dump --schema-only --no-owner --no-acl --disable-triggers \
   --no-comments --no-publications --no-security-labels \
-  -N snapshot -N sqitch -N sqitch-data \
+  -N snapshot -N sqitch -N sqitch-data -N partman \
+  -T '*_temp' -T 'blocks_p*' -T 'validator_transactions_p*' \
+  -T 'validator_transaction_players_p*' -T '*_template' \
   --no-subscriptions --no-tablespaces \
   --host "${POSTGRES_HOST:-localhost}" \
   --username "${POSTGRES_USER:-postgres}" \
-  "${POSTGRES_DB:-postgres}"
+  "${POSTGRES_DB:-postgres}" | \
+  # remove the CREATE EXTENSION lines
+  sed '/^CREATE EXTENSION/d'
 
