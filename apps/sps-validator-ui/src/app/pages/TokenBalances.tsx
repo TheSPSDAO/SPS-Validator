a3930a5c25dabd0cec263ac2dbf0e68da12a61ea
Jef <jgeerinckx@hotmail.com>
2025-03-17 02:12:47 +0100

    Style update on updated TokenBalances page
    Darkmode and responsiveness added to new elements after update on TokenBalances page

--- apps/sps-validator-ui/src/app/pages/TokenBalances.tsx
+++ apps/sps-validator-ui/src/app/pages/TokenBalances.tsx
@@ -2,9 +2,10 @@ import { Card, CardBody, Checkbox, Select, Spinner, Tab, TabPanel, Tabs, TabsBod
 import { useMemo, useRef, useState } from 'react';
 import { DefaultService } from '../services/openapi';
 import { usePromise } from '../hooks/Promise';
-import { Table, TableBody, TableCell, TableColumn, TableHead, TablePager, TableRow, GradientOverflow } from '../components/Table';
+import { Table, TableBody, TableCell, TableColumn, TableHead, TablePager, TableRow, GradientOverflow, TableHeader } from '../components/Table';
 import { localeNumber } from '../components/LocaleNumber';
 import { useSpinnerColor } from '../hooks/SpinnerColor'
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
 const tokens = ['SPS', 'SPSP', 'SPS_TOTAL', 'LICENSE', 'ACTIVATED_LICENSE', 'LICENSE_TOTAL', 'RUNNING_LICENSE'];
 
@@ -12,10 +13,11 @@ function TokenBalancesTable({ token }: { token: string }) {
     const [page, setPage] = useState(0);
     const [limit, setLimit] = useState(10); // TODO: Add a limit selector
     const [systemAccounts, setSystemAccounts] = useState(false);
-    const [count, isLoadingCount] = usePromise(() => DefaultService.getExtendedBalancesByToken(token, 0, 0, systemAccounts), [token, systemAccounts]);
-    const [balances, isLoading] = usePromise(() => DefaultService.getExtendedBalancesByToken(token, limit, page * limit, systemAccounts), [token, page, limit, systemAccounts]);
+    const [count, isLoadingCount] = usePromise(() => DefaultService.getBalancesByToken(token, 0, 0, systemAccounts), [token, systemAccounts]);
+    const [balances, isLoading] = usePromise(() => DefaultService.getBalancesByToken(token, limit, page * limit, systemAccounts), [token, page, limit, systemAccounts]);
     const spinnerColor = useSpinnerColor("blue")
     const containerRef = useRef<HTMLDivElement | null>(null);
+
 
     const toggleSystemAccounts = (value: boolean) => {
         setSystemAccounts(value);
@@ -64,8 +66,9 @@ function TokenBalancesTable({ token }: { token: string }) {
 
 function TokenSupplyTable({ token }: { token: string }) {
     const [supply, isLoading] = usePromise(() => DefaultService.getExtendedTokenSupply(token), [token]);
+    const spinnerColor = useSpinnerColor("blue")
     if (isLoading) {
-        return <Spinner className="w-full" />;
+        return <Spinner className="w-full" color={spinnerColor}/>;
     } else if (!supply) {
         return <Typography variant="h6">No supply information available</Typography>;
     } else if (token === 'SPS') {
@@ -76,6 +79,7 @@ function TokenSupplyTable({ token }: { token: string }) {
 }
 
 function ObjectTable({ obj }: { obj: Record<string, any> }) {
+    const containerRef = useRef<HTMLDivElement | null>(null);
     const format = (value: any) => {
         if (typeof value === 'object') {
             return JSON.stringify(value);
@@ -85,22 +89,22 @@ function ObjectTable({ obj }: { obj: Record<string, any> }) {
         return value;
     };
     return (
-        <Table className="w-full border-2 border-gray-200">
-            <TableHead>
-                <TableRow>
-                    <TableColumn>Property</TableColumn>
-                    <TableColumn>Value</TableColumn>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {Object.entries(obj ?? {}).map(([key, value]) => (
-                    <TableRow key={key}>
-                        <TableCell>{key}</TableCell>
-                        <TableCell>{format(value)}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+        <div className="relative">
+            <div ref={containerRef} className="overflow-x-auto">
+                <Table className="w-full border-2 p-4 border-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-300">
+                    <TableHeader columns={["Property", "Value"]}/>
+                    <TableBody>
+                        {Object.entries(obj ?? {}).map(([key, value]) => (
+                            <TableRow key={key}  className="dark:border-gray-300">
+                                <TableCell>{key}</TableCell>
+                                <TableCell>{format(value)}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            <GradientOverflow containerRef={containerRef} isLoading={false} />
+        </div>
     );
 }
 
@@ -128,6 +132,7 @@ type SpsTokenSupply = {
     };
 };
 function SpsSupplyTable({ supply }: { supply: SpsTokenSupply }) {
+    
     const summary = useMemo(() => {
         return {
             minted: supply.minted,
@@ -138,45 +143,57 @@ function SpsSupplyTable({ supply }: { supply: SpsTokenSupply }) {
         };
     }, [supply]);
     return (
-        <Tabs value="sum">
-            <TabsHeader>
-                <Tab value="sum">Summary</Tab>
-                <Tab value="offChain">Off Chain</Tab>
-                <Tab value="reserve">Reserve</Tab>
-                <Tab value="rewardPools">Reward Pools</Tab>
-            </TabsHeader>
-            <TabsBody>
-                <TabPanel value="sum">
-                    <ObjectTable obj={summary}></ObjectTable>
-                </TabPanel>
-                <TabPanel value="offChain">
-                    <ObjectTable obj={supply.off_chain}></ObjectTable>
-                </TabPanel>
-                <TabPanel value="reserve">
-                    <ObjectTable obj={supply.reserve}></ObjectTable>
-                </TabPanel>
-                <TabPanel value="rewardPools">
-                    <ObjectTable obj={supply.reward_pools}></ObjectTable>
-                </TabPanel>
-            </TabsBody>
-        </Tabs>
+        <div className="relative">
+            <div className="overflow-x-auto">
+                <Tabs value="sum">
+                    <TabsHeader  className="flex items-center dark:bg-gray-300 dark:text-gray-800" indicatorProps={{className: "transition-colors delay-200 duration-400 dark:bg-blue-800"}}>
+                        <Tab value="sum" activeClassName="dark:text-gray-300 transition-colors delay-200 duration-400" className="transition-colors delay-200 duration-400">Summary</Tab>
+                        <Tab value="offChain" activeClassName="dark:text-gray-300 transition-colors delay-200 duration-400" className="transition-colors delay-200 duration-400">Off Chain</Tab>
+                        <Tab value="reserve" activeClassName="dark:text-gray-300 transition-colors delay-200 duration-400" className="transition-colors delay-200 duration-400">Reserve</Tab>
+                        <Tab value="rewardPools" activeClassName="dark:text-gray-300 transition-colors delay-200 duration-400" className="transition-colors delay-200 duration-400">Reward Pools</Tab>
+                    </TabsHeader>
+                    <TabsBody>
+                        <TabPanel value="sum">
+                            <ObjectTable obj={summary}></ObjectTable>
+                        </TabPanel>
+                        <TabPanel value="offChain">
+                            <ObjectTable obj={supply.off_chain}></ObjectTable>
+                        </TabPanel>
+                        <TabPanel value="reserve">
+                            <ObjectTable obj={supply.reserve}></ObjectTable>
+                        </TabPanel>
+                        <TabPanel value="rewardPools">
+                            <ObjectTable obj={supply.reward_pools}></ObjectTable>
+                        </TabPanel>
+                    </TabsBody>
+                </Tabs>
+            </div>
+        </div>
     );
 }
-
 
 export function TokenBalances() {
     const [token, setToken] = useState(tokens[0]);
     return (
-        <div className="grid gap-6">
-            <Card className="col-span-full dark:bg-gray-800 dark:text-gray-300 dark:shadow-none">
+        <div className="grid gap-6 w-full">
+            <Card className="w-full col-span-full dark:bg-gray-800 dark:text-gray-300 dark:shadow-none">
                 <CardBody>
                     <Typography variant="h5" color="blue-gray" className="mb-2 dark:text-gray-200">
                         Token Information
                     </Typography>
-                    <div className="mt-4 flex 2xl:max-w-96 2xl:w-1/4 lg:w-2/3 md:w-full">
-                        <Select label="Token" value={token} onChange={(val) => setToken(val ?? tokens[0])} className="dark:bg-gray-300 dark:text-gray-800" >
+                    <div className="mt-4 flex max-w-96 dark:bg-gray-800 dark:text-gray-300">
+                        <Select 
+                            label="Token" 
+                            value={token} 
+                            onChange={(val) => setToken(val ?? tokens[0])} 
+                            className="dark:bg-gray-800 dark:text-gray-300 border-x-gray-300 border-b-gray-300"
+                            labelProps={{className: "dark:text-gray-300 dark:peer-disabled:before:border-transparent dark:peer-disabled:after:border-transparent dark:before:border-gray-300 dark:after:border-gray-300 "}} 
+                            containerProps={{className: "dark:bg-gray-800"}} 
+                            menuProps={{className: "dark:bg-gray-800 dark:text-gray-300 dark:border-gray-300"}} 
+                            arrow={<ChevronDownIcon className="w-5 h-5 dark:fill-gray-300 dark:text-gray-300" />}
+                        >
                             {tokens.map((token) => (
-                                <Option key={token} value={token} className="transition-colors delay-300 duration-500 dark:text-gray-300">
+                                <Option key={token} value={token} className="dark:text-gray-300 dark:bg-gray-800 dark:hover:bg-gray-300 dark:hover:text-gray-800" >
                                     {token}
                                 </Option>
                             ))}
@@ -184,23 +201,23 @@ export function TokenBalances() {
                     </div>
                 </CardBody>
             </Card>
-            <div className="grid xl:grid-cols-2 gap-6 col-span-full">
-                <Card className="col-span-1">
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+                <Card className="w-full col-span-1 dark:bg-gray-800 dark:text-gray-300 dark:shadow-none">
                     <CardBody>
-                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                        <Typography variant="h5" color="blue-gray" className="mb-6 dark:text-gray-200">
                             Account Balances
                         </Typography>
-                        <div className="mt-4">
+                        <div>
                             <TokenBalancesTable token={token} />
                         </div>
                     </CardBody>
                 </Card>
-                <Card className="col-span-1">
+                <Card className="w-full col-span-1 dark:bg-gray-800 dark:text-gray-300 dark:shadow-none">
                     <CardBody>
-                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                        <Typography variant="h5" color="blue-gray" className="mb-6 dark:text-gray-200">
                             Token Supply
                         </Typography>
-                        <div className="mt-4">
+                        <div>
                             <TokenSupplyTable token={token} />
                         </div>
                     </CardBody>
