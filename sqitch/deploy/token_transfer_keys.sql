8499cc79f188785fc5c572f58bcb1d3c067bd900
smoyer-sl <smoyer@splinterlands.com>
2025-11-25 10:16:19 -0500

    Use latest post snapshot restore function in token transfer keys

--- sqitch/deploy/token_transfer_keys.sql
+++ sqitch/deploy/token_transfer_keys.sql
@@ -18,52 +18,59 @@ CREATE TABLE IF NOT EXISTS snapshot.token_transfer_keys (
     block_time timestamp without time zone NOT NULL
 );
 
-CREATE OR REPLACE FUNCTION snapshot.pre_snapshot_restore(p_data_schema text DEFAULT :'APP_SCHEMA'::text)
-    RETURNS void
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-AS $BODY$
-BEGIN
-
-	PERFORM set_config('search_path', regexp_replace(p_data_schema ||', public', '[^\w ,]', '', 'g'), true);
-
-	RAISE NOTICE 'Data source schema: %', p_data_schema;
-
-    -- Delete any pre-existing snapshot data
-    TRUNCATE TABLE snapshot.token_unstaking;
-    TRUNCATE TABLE snapshot.hive_accounts;
-    TRUNCATE TABLE snapshot.balances;
-    TRUNCATE TABLE snapshot.balance_history;
-    TRUNCATE TABLE snapshot.staking_pool_reward_debt;
-    TRUNCATE TABLE snapshot.validator_votes;
-    TRUNCATE TABLE snapshot.validators;
-    TRUNCATE TABLE snapshot.validator_vote_history;
-    TRUNCATE TABLE snapshot.blocks;
-    TRUNCATE TABLE snapshot.validator_transactions;
-    TRUNCATE TABLE snapshot.validator_transaction_players;
-    TRUNCATE TABLE snapshot.price_history;
-    TRUNCATE TABLE snapshot.active_delegations;
-    TRUNCATE TABLE snapshot.validator_check_in;
-    TRUNCATE TABLE snapshot.promise;
-    TRUNCATE TABLE snapshot.promise_history;
-    TRUNCATE TABLE snapshot.config;
-    TRUNCATE TABLE snapshot.token_transfer_keys;
-END;
-$BODY$;
-
-
 CREATE OR REPLACE FUNCTION snapshot.post_snapshot_restore(p_data_schema text DEFAULT :'APP_SCHEMA'::text)
     RETURNS void
     LANGUAGE 'plpgsql'
     COST 100
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
+DECLARE
+    v_min_block_number    bigint;
+    v_max_block_number    bigint;
+    v_block_partition_ids bigint[];
 BEGIN
 
 	PERFORM set_config('search_path', regexp_replace(p_data_schema ||', public', '[^\w ,]', '', 'g'), true);
 
 	RAISE NOTICE 'Data source schema: %', p_data_schema;
+
+    -- special handling for partitioned tables
+    SELECT MIN(block_num) INTO v_min_block_number FROM snapshot.blocks;
+    IF v_min_block_number IS NULL THEN
+        v_min_block_number := 0;
+    END IF;
+
+    SELECT MAX(block_num) INTO v_max_block_number FROM snapshot.blocks;
+    IF v_max_block_number IS NULL THEN
+        v_max_block_number := 432000 * 5;
+    END IF;
+
+    SELECT
+        array(
+            SELECT generate_series(
+                v_min_block_number,
+                v_max_block_number,
+                432000
+            )
+        )
+    INTO v_block_partition_ids;
+
+    -- create the new partitions first
+    PERFORM partman.create_partition_id(p_data_schema || '.blocks', v_block_partition_ids);
+    PERFORM partman.create_partition_id(p_data_schema || '.validator_transactions', v_block_partition_ids);
+    PERFORM partman.create_partition_id(p_data_schema || '.validator_transaction_players', v_block_partition_ids);
+
+    -- run maintenance to clean up any old partitions and add new ones.
+    -- note: if the node is an archive node, we will have some empty partitions at block 0. this shouldnt cause any issues but
+    -- it'd be nice if we could get rid of them in the future
+    PERFORM partman.run_maintenance(p_analyze := TRUE, p_jobmon := FALSE);
+
+    INSERT INTO blocks SELECT * FROM snapshot.blocks;
+    INSERT INTO validator_transactions SELECT * FROM snapshot.validator_transactions;
+    INSERT INTO validator_transaction_players SELECT * FROM snapshot.validator_transaction_players;
+
+    -- partman will premake the remaining future partitions and drop any old ones
+    PERFORM partman.run_maintenance(p_analyze := TRUE, p_jobmon := FALSE);
 
     INSERT INTO active_delegations SELECT * FROM snapshot.active_delegations;
     INSERT INTO balance_history SELECT * FROM snapshot.balance_history;
@@ -75,9 +82,6 @@ BEGIN
     INSERT INTO validator_votes SELECT * FROM snapshot.validator_votes;
     INSERT INTO validator_vote_history SELECT * FROM snapshot.validator_vote_history;
     INSERT INTO validators SELECT * FROM snapshot.validators;
-    INSERT INTO blocks SELECT * FROM snapshot.blocks;
-    INSERT INTO validator_transactions SELECT * FROM snapshot.validator_transactions;
-    INSERT INTO validator_transaction_players SELECT * FROM snapshot.validator_transaction_players;
     INSERT INTO token_unstaking SELECT * FROM snapshot.token_unstaking;
     INSERT INTO validator_check_in SELECT * FROM snapshot.validator_check_in;
     INSERT INTO token_transfer_keys SELECT * FROM snapshot.token_transfer_keys;
@@ -110,7 +114,6 @@ BEGIN
 
 END;
 $BODY$;
-
 
 CREATE OR REPLACE FUNCTION snapshot.freshsnapshot(
 	p_archive_flag boolean DEFAULT false,
